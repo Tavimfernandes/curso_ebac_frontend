ae2b8e459befd41d9ee8524c1c2fe88c1676fdfa
Octavio <octaviofp2003@outlook.com>
2023-02-05 18:53:19 -0300

    Exercicio jquery

--- README.md
+++
@@ -1,9 +0,0 @@
-# curso_ebac_frontend
-Repositório criado para atividades do curso de Engenheiro Front-End da EBAC
-
-Branches:
-
-  Exercicio_git: Para a atividade do módulo 02 - GIT
-  Exercicio_html: Para a atividade do módulo 03 - HTML
-  Exercicio_css: Para a atividade do módulo 04 - CSS
-  Exercicio_html_js: Para a atividade do módulo 06 - JavaScript e HTML
--- main.js
+++ main.js
@@ -1,43 +1,29 @@
-const form = document.getElementById('form-numero');
-const numeroA = document.getElementById('numeroUm');
-const numeroB = document.getElementById('numeroDois');
-let formEValido = false;
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
 
-function validaNumero() {
-    let eMaior = numeroB.value > numeroA.value ? true : false
-    return eMaior == true;
-}
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
 
-form.addEventListener('submit', function (e){
-    e.preventDefault();
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const nomeNovaTarefa = $('#nome-nova-tarefa').val();
+        const novaTarefa = $('<li style="display: none"></li>');
+        $(`
+            <div class="tarefa">
+                <p>${nomeNovaTarefa}</p>
+            </div>
+        `).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
+        $(novaTarefa).fadeIn(1000);
+        $('#nome-nova-tarefa').val('')
 
-    const mensagemSucesso = `Sucesso! o número <b>${numeroDois.value}</b> é maior que <b>${numeroUm.value}</b>!`;    
+        $('.tarefa').click(function(){
+            $(this).addClass('efeitoLinha');
+        })
+    })
 
-    formEValido = validaNumero()
-    if (formEValido) {
-        const containerMensagemSucesso = document.querySelector('.sucess-message');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block';
-        
-        numeroA.value = '';
-        numeroB.value = '';
-
-    } else {
-        numeroB.style.border = '1px solid red';
-        document.querySelector('.error-message').style.display = 'block';
-    }
+    
 })
-
-numeroB.addEventListener('keyup', function(e){
-    console.log(e.target.value);
-    formEValido = validaNumero();
-
-    if (!formEValido) {
-        numeroB.classList.add('error');
-        document.querySelector('.error-message').style.display = 'block';
-    } else {
-        numeroB.classList.remove('error');
-        document.querySelector('.error-message').style.display = 'none';
-    }
-}
-)
