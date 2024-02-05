0ce3be723a0ab80d09826f752ffaa3fd4e6b5e3c
Octavio Fernandes Pereira <octaviofp2003@outlook.com>
2024-02-05 10:01:24 -0300

    exercicio m20

--- exercicio.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const multiplicacao = (numeroA, numeroB) => numeroA * numeroB;
-console.log(multiplicacao(10, 20));
-function saudacao(nome) {
-    return `Olá ${nome}!`;
-}
-console.log(saudacao('Octávio'));
--- exercicio.ts
+++
@@ -1,7 +0,0 @@
-const multiplicacao = (numeroA: number, numeroB: number): number => numeroA * numeroB;
-console.log(multiplicacao(10, 20))
-
-function saudacao(nome: string){
-    return `Olá ${nome}!`
-}
-console.log(saudacao('Octávio'))
